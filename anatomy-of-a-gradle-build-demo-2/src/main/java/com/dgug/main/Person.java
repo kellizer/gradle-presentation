50d977330b680372c3bc18c6051934dc228fd628
moonsync <admin@moonsync.com>
2013-06-11 21:13:01 +0100

    More Changes for a Demo

---
+++ anatomy-of-a-gradle-build-demo-2/src/main/java/com/dgug/main/Person.java
@@ -0,0 +1,49 @@
+package com.dgug.main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kellizer
+ * Date: 08/06/2013
+ * Time: 13:24
+ * A person who has knowledge of age.
+ */
+public class Person {
+
+    private int myAge;
+
+    public Person(int myAge) {
+        this.myAge = myAge;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0)
+            System.out.println("Saying Hello From Person Main.");
+        else
+        {
+            System.out.println("Hello " + args[0] +", nice to meet you.");
+        }
+
+    }
+
+    /**
+     * Give you my age.
+     *
+     * @return - my age.
+     */
+    public int getMyAge() {
+        return myAge;
+    }
+
+    /**
+     * I'll tell you if you are older than me.
+     *
+     * @param yourAge - what age you age
+     * @return true if you are older, false if you are younger of the same age.
+     */
+    public boolean amIOlderThanYou(Person yourAge) {
+
+        return myAge > yourAge.getMyAge();
+
+    }
+
+}
