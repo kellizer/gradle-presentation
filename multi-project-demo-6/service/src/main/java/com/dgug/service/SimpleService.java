cbeac67db01ea2f029bc49425645a8e82640f9ba
moonsync <admin@moonsync.com>
2013-06-11 21:08:58 +0100

    More Changes for the demo

---
+++ multi-project-demo-6/service/src/main/java/com/dgug/service/SimpleService.java
@@ -0,0 +1,10 @@
+package com.dgug.service;
+
+
+public class SimpleService {
+
+    @Override
+    public String toString() {
+        return "simple service";
+    }
+}
---
+++ multi-project-demo-6/shared/src/main/java/com/dgug/shared/PersonShared.java
@@ -0,0 +1,12 @@
+package com.dgug.shared;
+
+/**
+ * This is a Shared Person
+ */
+public class PersonShared {
+
+    @Override
+    public String toString() {
+        return "Im a shared Person";
+    }
+}
