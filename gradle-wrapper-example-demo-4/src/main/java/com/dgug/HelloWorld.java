eafafef2177f950606d9329e8f5ce752a438626a
moonsync <admin@moonsync.com>
2013-06-11 21:10:46 +0100

    More Changes for the Demo

---
+++ gradle-wrapper-example-demo-4/src/main/java/com/dgug/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.dgug;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kellizer
+ * Date: 20/05/2013
+ * Time: 20:34
+ * Outputs a statement to the standard out..
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello World from Java Wrapper");
+    }
+}
